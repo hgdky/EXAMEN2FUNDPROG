c50d0ddc2f6d1738826b3eb6a5ddbaa879e95e5e
hgdky <82120279+hgdky@users.noreply.github.com>
2021-06-22 19:36:01 -0500

    Add files via upload

---
+++ Ejercicio4.1/src/App.java
@@ -0,0 +1,14 @@
+
+
+
+import pe.edu.upeu.ejercicio31;
+
+public class App {
+
+
+
+    public static void main(String[] args) {
+        ejercicio31 obj=new ejercicio31();//obj es un objeto, usar siempre que haya que usar los metodos de adentro
+        obj.resultado1();
+    }
+}
---
+++ Ejercicio4.1/src/pe/edu/upeu/ejercicio31.java
@@ -0,0 +1,29 @@
+package pe.edu.upeu.MigHuay;
+//import java.util.*;
+
+public class ejercicio31 {
+
+
+    public static void main(){
+
+        //Scanner entrada=new Scanner (System.in);
+        int salario=1500;
+        int porcentaje=10;
+       // int años = 6;
+        int resultado;
+
+    public int abc(int resultado){
+        resultado=salario/porcentaje;
+
+        System.out.println(resultado);
+
+    }
+
+    }
+    
+
+    
+    
+        
+    } 
+
